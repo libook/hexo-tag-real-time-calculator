2ec4ab90968511587af34bdd4acf5a8f041d0be8
libook <libook7@gmail.com>
2019-02-03 15:03:41 +0800

    Fix white space issue. Fix same uid issue. Usable readme.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # hexo-tag-real-time-calculator
+
 A hexo plugin for inserting values which need to be calculated on real time.
+
+This plugin is very easy to use.
+
+#### Installation
+
+Locate to the root path of your hexo blog.
+
+```bash
+npm i hexo-tag-real-time-calculator --save
+```
+
+Done!
+
+#### Usage
+
+```markdown
+{% calc '<JS code for returning result.>' %}
+```
+For example:
+
+```markdown
+I'am {% calc "(new Date()).getFullYear()-1992" %} years old.
+```
+Or
+
+```markdown
+I'am {% calc '(new Date()).getFullYear()-1992' %} years old.
+```
+
+The output is(in 2019):
+
+```html
+I'am 27 years old.
+```
--- index.js
+++ index.js
@@ -2,15 +2,24 @@
 
 /**
  * Syntax:
- *  {% calc <JS code for returning result.> %}
+ *  {% calc '<JS code for returning result.>' %}
  */
 hexo.extend.tag.register('calc', (args) => {
-    const uid = Date.now();
+    if (!global._HEXO_TAG_REAL_TIME_CALCULATOR_UID) {
+        global._HEXO_TAG_REAL_TIME_CALCULATOR_UID = 0;
+    }
+
+    const uid = ++global._HEXO_TAG_REAL_TIME_CALCULATOR_UID;
     const id = `tag-calc-${uid}`;
-    return `
-        <span id="${id}"></span>
-        <script type="application/javascript">
-            document.getElementById('${id}').innerHTML = eval(${args[0]});
-        </script>
-    `;
+
+    /**
+     * These "<!-- -->" are important!
+     * For solving the white space issue.
+     */
+    return `<!--
+        ---><span id="${id}" style=""></span><!--
+        ---><script type="application/javascript">
+                document.getElementById('${id}').innerHTML = eval(${args[0]});
+            </script><!--
+        --->`;
 }, { "ends": false });
